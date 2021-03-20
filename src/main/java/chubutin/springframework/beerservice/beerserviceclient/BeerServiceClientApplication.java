7e004b71744d9d4c54e7e983df011ea3aa389fa0
Ramiro Pugh <rampugh@gmail.com>
2021-03-20 13:32:24 -0300

    First commit

---
+++ src/main/java/chubutin/springframework/beerservice/beerserviceclient/BeerServiceClientApplication.java
@@ -0,0 +1,13 @@
+package chubutin.springframework.beerservice.beerserviceclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerServiceClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerServiceClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chubutin/springframework/beerservice/beerserviceclient/BeerServiceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package chubutin.springframework.beerservice.beerserviceclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerServiceClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
